42898b210fb8c1da900fa16b2d90b3a7f0dc3ab6
PeoshPeush <PeoshPeush@gmail.com>
2023-03-07 17:11:42 +0900

    ...
    .

--- MyProject/Source/MyProject/Widget_Inventory.cpp
+++ MyProject/Source/MyProject/Widget_Inventory.cpp
@@ -21,22 +21,22 @@ void UWidget_Inventory::NativeConstruct()
 
 bool UWidget_Inventory::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
 {
-	Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
-
-	UDragWidget* DragWidgetResult = Cast<UDragWidget>(InOperation);
-
-	if (!IsValid(DragWidgetResult))
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Cast returned null"));
-		return false;
-	}
-
-	const FVector2D DragWindowOffset = InGeometry.AbsoluteToLocal(InDragDropEvent.GetScreenSpacePosition());
-	const FVector2D DragWindowOffsetResult = DragWindowOffset - DragWidgetResult->DragOffset;
-
-	DragWidgetResult->WidgetReference->AddToViewport();
-	DragWidgetResult->WidgetReference->SetVisibility(ESlateVisibility::Visible);
-	DragWidgetResult->WidgetReference->SetPositionInViewport(DragWindowOffsetResult, false);
+	//Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+	//
+	//UDragWidget* DragWidgetResult = Cast<UDragWidget>(InOperation);
+	//
+	//if (!IsValid(DragWidgetResult))
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Cast returned null"));
+	//	return false;
+	//}
+	//
+	//const FVector2D DragWindowOffset = InGeometry.AbsoluteToLocal(InDragDropEvent.GetScreenSpacePosition());
+	//const FVector2D DragWindowOffsetResult = DragWindowOffset - DragWidgetResult->DragOffset;
+	//
+	//DragWidgetResult->WidgetReference->AddToViewport();
+	//DragWidgetResult->WidgetReference->SetVisibility(ESlateVisibility::Visible);
+	//DragWidgetResult->WidgetReference->SetPositionInViewport(DragWindowOffsetResult, false);
 	return true;
 }
 
