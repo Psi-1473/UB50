28e5dcff888d25ff9d7443ae8c1fe1e1a7cdb9e8
PeoshPeush <PeoshPeush@gmail.com>
2023-04-06 22:51:39 +0900

    퀘스트 json 연동 끝

--- MyProject/Source/MyProject/Private/Manager_Quest.cpp
+++ MyProject/Source/MyProject/Private/Manager_Quest.cpp
@@ -19,33 +19,32 @@ void UManager_Quest::LoadQuestData()
 
 	if (FJsonSerializer::Deserialize(jsonReader, jsonObj) && jsonObj.IsValid())
 	{
-		//UE_LOG(LogTemp, Log, TEXT("Json Deserialized"));
-		//
-		//const TArray<TSharedPtr<FJsonValue>>* jsonQuestItems;
-		//if (jsonObj->TryGetArrayField(TEXT("quests"), jsonQuestItems))
-		//{
-		//	for (int i = 0; i < jsonQuestItems->Num(); i++)
-		//	{
-		//		TSharedPtr<FJsonObject> jsonItem = jsonQuestItems[i]->AsObject();
-		//		Quest q;
-		//		jsonItem->TryGetNumberField(TEXT("Id"), q.Id);
-		//		jsonItem->TryGetNumberField(TEXT("NpcId"), q.NpcId);
-		//		jsonItem->TryGetNumberField(TEXT("CompleteNpcId"), q.CompleteNpcId);
-		//		jsonItem->TryGetStringField(TEXT("Type"), q.Type);
-		//		jsonItem->TryGetStringField(TEXT("Name"), q.Name);
-		//		jsonItem->TryGetStringField(TEXT("Sub"), q.Sub);
-		//		jsonItem->TryGetStringField(TEXT("ConditionSub"), q.ConditionSub);
-		//		jsonItem->TryGetNumberField(TEXT("Gold"), q.Gold);
-		//		jsonItem->TryGetNumberField(TEXT("Exp"), q.Exp);
-		//		jsonItem->TryGetBoolField(TEXT("Locked"), q.Locked);
-		//		jsonItem->TryGetBoolField(TEXT("Cleared"), q.Cleared);
-		//		jsonItem->TryGetNumberField(TEXT("TargetId"), q.TargetId);
-		//		jsonItem->TryGetNumberField(TEXT("TargetNum"), q.TargetNum);
-		//		jsonItem->TryGetNumberField(TEXT("Next"), q.Next);
-		//		
-		//		Quests.Add(q.Id, q);
-		//	}
-		//}
+		UE_LOG(LogTemp, Log, TEXT("Json Deserialized"));
+
+			TArray<TSharedPtr<FJsonValue>> jsonQuestItems = jsonObj->GetArrayField(TEXT("quests"));
+			for (int i = 0; i < jsonQuestItems.Num(); i++)
+			{
+				TSharedPtr<FJsonObject> jsonItem = jsonQuestItems[i]->AsObject();
+				Quest q;
+				jsonItem->TryGetNumberField(TEXT("Id"), q.Id);
+				jsonItem->TryGetNumberField(TEXT("NpcId"), q.NpcId);
+				jsonItem->TryGetNumberField(TEXT("CompleteNpcId"), q.CompleteNpcId);
+				jsonItem->TryGetStringField(TEXT("Type"), q.Type);
+				jsonItem->TryGetStringField(TEXT("Name"), q.Name);
+				jsonItem->TryGetStringField(TEXT("Sub"), q.Sub);
+				jsonItem->TryGetStringField(TEXT("ConditionSub"), q.ConditionSub);
+				jsonItem->TryGetNumberField(TEXT("Gold"), q.Gold);
+				jsonItem->TryGetNumberField(TEXT("Exp"), q.Exp);
+				jsonItem->TryGetBoolField(TEXT("Locked"), q.Locked);
+				jsonItem->TryGetBoolField(TEXT("Cleared"), q.Cleared);
+				jsonItem->TryGetNumberField(TEXT("TargetId"), q.TargetId);
+				jsonItem->TryGetNumberField(TEXT("TargetNum"), q.TargetNum);
+				jsonItem->TryGetNumberField(TEXT("Next"), q.Next);
+
+				Quests.Add(q.Id, q);
+			}
+
+		UE_LOG(LogTemp, Log, TEXT("Quest Files Saved %d %d"), Quests[0].Gold, Quests[1].Gold);
 	}
 
 
