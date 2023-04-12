e7cfe9468db28288d51cd2c154e42452dcc5ccd8
PeoshPeush <PeoshPeush@gmail.com>
2023-04-12 23:16:19 +0900

    ..

---
+++ MyProject/Source/MyProject/Public/Widget_PlayerQuest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_PlayerQuest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UWidget_PlayerQuest : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ MyProject/Source/MyProject/Public/Widget_PlayerQuestList.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_PlayerQuestList.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UWidget_PlayerQuestList : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
